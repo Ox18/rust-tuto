05fa6525e9357d7bdc1f0993813954ab231924de
Alex <Wilmer.DelgadoAlama09@gmail.com>
2022-03-02 08:19:25 -0500

    first commit

---
+++ target/debug/deps/hola_mundo-a0b453ec1b467b56.d
@@ -0,0 +1,5 @@
+/Users/Conauti/Downloads/Wilmer - Docs/hola_mundo/target/debug/deps/hola_mundo-a0b453ec1b467b56.rmeta: src/main.rs
+
+/Users/Conauti/Downloads/Wilmer - Docs/hola_mundo/target/debug/deps/hola_mundo-a0b453ec1b467b56.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/hola_mundo-f58dc2729410ceb9.d
@@ -0,0 +1,5 @@
+/Users/Conauti/Downloads/Wilmer - Docs/hola_mundo/target/debug/deps/hola_mundo-f58dc2729410ceb9.rmeta: src/main.rs
+
+/Users/Conauti/Downloads/Wilmer - Docs/hola_mundo/target/debug/deps/hola_mundo-f58dc2729410ceb9.d: src/main.rs
+
+src/main.rs:
